3b1a93955f40757809559d0679d76a56f5ed7788
nwnel-017 <nnelson19@ewu.edu>
2026-03-06 14:02:57 -0400

    contact page added

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
+import InstagramIcon from "~/components/icons/InstagramIcon.vue";
+
 definePageMeta({
   layout: "default",
 });
 </script>
 
 <template>
-  <h1>Contact</h1>
+  <div class="verticalContent">
+    <h1>Please feel free to reach out to me at any time!</h1>
+    <div class="contactInfo">
+      <div class="contactItem">
+        <h2>Email</h2>
+        <p>jamiestambolie@gmail.com</p>
+        <span>Click here to follow me on Instagram!</span>
+        <InstagramIcon />
+      </div>
+    </div>
+  </div>
 </template>
